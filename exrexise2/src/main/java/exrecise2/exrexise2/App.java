874948571e43c30c7f0e8faa8928f71a0c742b83
רואי דולב < @ROIs-lap>
2024-01-14 14:12:42 +0200

    calculate-simple-2

---
+++ exrexise2/src/main/java/exrecise2/exrexise2/App.java
@@ -0,0 +1,13 @@
+package exrecise2.exrexise2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
